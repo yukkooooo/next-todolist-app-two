93715b67e123d204c9d6caa98b91b7acc8f4a4c6
yukkooooo <calm.and.clear.02@gmail.com>
2024-07-09 02:09:07 +0900

    機能追加

--- src/app/todos/[id]/page.tsx
+++ src/app/todos/[id]/page.tsx
@@ -4,35 +4,49 @@ import Link from 'next/link';
 import { useRecoilValue } from 'recoil';
 import { AddTodo } from '@/components/AddTodo';
 import { todoState } from '@/atoms/todoState';
-
-
-
+import { useState, useEffect } from 'react';
 
 export default function Page({ params }: { params: { id: string } }) {
   const todos = useRecoilValue(todoState);
+  const [input, setInput] = useState('');
+  const [savedInput, setSavedInput] = useState('');
   const todo = todos.find((todo) => todo.id === Number(params.id));
+
+  useEffect(() => {
+    if (todo) {
+      const savedTitle = localStorage.getItem(`todo-${todo.id}`);
+      if (savedTitle) {
+        setInput(savedTitle);
+        setSavedInput(savedTitle);
+      } else {
+        setInput(todo.title);
+        setSavedInput(todo.title);
+      }
+    }
+  }, [todo]);
+
+  const saveTodo = () => {
+    setSavedInput(input); // 入力した内容を保存
+    if (todo) {
+      localStorage.setItem(`todo-${todo.id}`, input); // ローカルストレージに保存
+    }
+    console.log("Saved todo:", input);
+  };
+
+
 
   return (
     <main className='container mx-auto max-w-md'>
-      <h1
-        className={`mt-2 mb-2 text-lg ${todo?.isCompleted ? 'line-through text-green-500' : 'text-slate-500'
-          }`}
-      >
-        {todo?.title}
+      <h1 className={`mt-2 mb-2 text-lg`}>
       </h1>
-      <h2 className='text-blue-400 hover:text-blue-600 '>内容</h2>
+      <h2 className='text-blue-400 hover:text-blue-600'>内容</h2>
 
       <div>
-        <AddTodo />
-
-
+        <AddTodo input={input} setInput={setInput} onClick={saveTodo} />
       </div>
-      <Link href='/' className='text-blue-400 hover:text-Red-600 '>
+      <Link href='/' className='text-blue-400 hover:text-red-600'>
         back
       </Link>
-
-
     </main>
   );
 }
-
