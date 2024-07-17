0581b212bcd1d7abbd7a23fb08be095199307172
yukkooooo <calm.and.clear.02@gmail.com>
2024-07-17 23:57:04 +0900

    修正

--- src/app/todos/[id]/page.tsx
+++ src/app/todos/[id]/page.tsx
@@ -1,16 +1,21 @@
 'use client';
 
 import Link from 'next/link';
-import { useRecoilValue } from 'recoil';
+import { useRecoilState } from 'recoil';
 import { AddTodo } from '@/components/AddTodo';
 import { todoState } from '@/atoms/todoState';
 import { useState, useEffect } from 'react';
+import { log } from 'console';
+import { title } from 'process';
+import { useRouter } from 'next/navigation'
 
 export default function Page({ params }: { params: { id: string } }) {
-  const todos = useRecoilValue(todoState);
+  const [todos, setTodos] = useRecoilState(todoState);
   const [input, setInput] = useState('');
   const [savedInput, setSavedInput] = useState('');
   const todo = todos.find((todo) => todo.id === Number(params.id));
+  const router = useRouter();
+  console.log(todo)
 
   useEffect(() => {
     if (todo) {
@@ -27,17 +32,40 @@ export default function Page({ params }: { params: { id: string } }) {
 
   const saveTodo = () => {
     setSavedInput(input); // 入力した内容を保存
+
+
+
+
+
     if (todo) {
-      localStorage.setItem(`todo-${todo.id}`, input); // ローカルストレージに保存
+      setTodos((prevTodos) =>
+        prevTodos.map((t) =>
+          t.id === todo.id ? { ...t, title: input } : t
+
+
+
+        )
+
+
+      );
+
+      console.log(todo);
+
+
+
+
+      // localStorage.setItem(`todo-${todo.id}`, input); // ローカルストレージに保存
     }
     console.log("Saved todo:", input);
+    router.push('/');
+
   };
 
 
 
   return (
     <main className='container mx-auto max-w-md'>
-      <h1 className={`mt-2 mb-2 text-lg`}>
+      <h1 className="mt-2 mb-2 text-lg">
       </h1>
       <h2 className='text-blue-400 hover:text-blue-600'>内容</h2>
 
